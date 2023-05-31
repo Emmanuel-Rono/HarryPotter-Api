e0062dc8eb1b7624bf2e41b9191bdb946dcabbe5
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-05-31 10:21:32 +0300

    Proceeding

--- app/src/main/java/com/emmanuel_rono/harry_potter_api/MainActivity.kt
+++ app/src/main/java/com/emmanuel_rono/harry_potter_api/MainActivity.kt
@@ -2,7 +2,13 @@ package com.emmanuel_rono.harry_potter_api
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.Recycler
 
+lateinit var recyclerview:RecyclerView
+lateinit var viewModel: ViewModel
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
